56a7bc59da9a42551f042245f69939eb8470668a
an-ju <an_ju@berkeley.edu>
2019-02-07 14:00:48 -0800

    separate model and migration from controller code.

--- template.rb
+++ template.rb
@@ -8,45 +8,9 @@ TO run the script, type ``ruby template.rb`` in command line.
 
 # load libraries we need 
 require 'sinatra'
-require 'active_record'
 require 'json'
 
-# tell active_record which database to connect to
-db_options = {adapter: 'sqlite3', database: 'todos_db'}
-ActiveRecord::Base.establish_connection(db_options)
-
-# write migration class for creating Todo table in database
-	### how do we write migration in rails?
-class CreateTodos < ActiveRecord::Migration[5.0]
-	def change
-		create_table :todos do |t|
-			t.string :description
-		end
-	end
-end
-
-# create Todo table by executing the function we just wrote
-	### how do apply migration in rails?
-	### why do we handle exception here?
-begin
-	CreateTodos.new.change
-rescue ActiveRecord::StatementInvalid
-	# it's probably OK
-end
-
-# create Todo class by inheriting from ActiveRecord::Base
-	### how do we write new class in Rails?
-	### why there's no setter and getter method (or attr_accessor)?
-class Todo < ActiveRecord::Base
-end
-
-# populate the database if it is empty (avoid running this piece of code twice)
-	### do you still remember this cleaner and simpler hash syntax?
-		# old version: new_hash = {:simon => "Talek", :lorem => "Ipsum"}
-if Todo.all.empty?
-	Todo.create(description: "prepare for discussion section")
-	Todo.create(description: "release cs169 hw3")
-end
+require './todo'
 
 # display all todos
 get '/todos' do 
